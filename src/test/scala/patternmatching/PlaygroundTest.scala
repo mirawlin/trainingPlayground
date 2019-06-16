b201942e8e995d68587cf5d430e99a41319b797c
Miranda Lin <mirandalin@nbnco.com.au>
2019-06-16 14:55:12 +1000

    Pattern matching Kata Questions and Test

--- src/test/scala/patternmatching/PlaygroundTest.scala
+++ src/test/scala/patternmatching/PlaygroundTest.scala
@@ -4,5 +4,26 @@ import org.scalatest.{Matchers, WordSpec}
 
 class PlaygroundTest extends WordSpec with Matchers {
 
+  "the function multiplyBy" should {
+    "multiply a number by nothing if multiplier is equal to 0" in {
 
+    }
+
+    "multiply a number by another number" in {
+
+    }
+
+    "return 0 when myNb is null" in {
+
+    }
+  }
+
+  "the fucntion multiis plyBy2" should {
+    "return 0 when nb is null" in {
+
+    }
+    "return the proper result otherwise" in {
+
+    }
+  }
 }
