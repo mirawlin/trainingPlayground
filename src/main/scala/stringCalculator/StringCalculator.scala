d380c0d4bfd92bbce0ad32dbb8f2e75f5e04f7bf
Miranda Lin <mirandalin@nbnco.com.au>
2019-06-16 15:18:30 +1000

    Answers to String Calc Q1 and 2

--- src/main/scala/stringCalculator/StringCalculator.scala
+++ src/main/scala/stringCalculator/StringCalculator.scala
@@ -1,5 +1,11 @@
 package stringCalculator
 
 object StringCalculator {
-  def add(numbers: String): Int = ???
+  def add(numbers: String): Int = {
+    if (numbers.isEmpty)
+      0
+    else if (numbers.contains(","))
+      numbers.split(",").map(_.toInt).sum
+    else numbers.toInt
+  }
 }
--- src/test/scala/stringCalculator/StringCalculatorTest.scala
+++ src/test/scala/stringCalculator/StringCalculatorTest.scala
@@ -1,7 +1,36 @@
 package stringCalculator
 
-import org.scalatest.WordSpec
+import org.scalatest.{Matchers, WordSpec}
 
-class StringCalculatorTest extends WordSpec {
+class StringCalculatorTest extends WordSpec with Matchers {
+  "function add" should {
+    "return 0 if empty string passed to it" in {
+      // WHEN
+      val result = StringCalculator.add("")
 
+      // THEN
+      result shouldEqual 0
+    }
+
+    "return the number when one string is passed" in {
+      val result = StringCalculator.add("2")
+
+      // THEN
+      result shouldEqual 2
+    }
+
+    "return the sum of the 2 numbers when there are 2 numbers in the string" in {
+      val result = StringCalculator.add("2,1")
+
+      // THEN
+      result shouldEqual 3
+    }
+
+    "return the sum of numbers when there are multiple numbers in the string" in {
+      val result = StringCalculator.add("2,1,5,0")
+
+      // THEN
+      result shouldEqual 8
+    }
+  }
 }
